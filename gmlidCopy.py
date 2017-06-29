e0243f7749b6f7dd67bad10b63d2b58176f7d770
jarped <jarle.pedersen@norconsult.com>
2017-06-29 10:48:54 +0200

    remove printing

--- gmlidCopy.py
+++ gmlidCopy.py
@@ -10,8 +10,6 @@ for featureType in tree.xpath('//ns0:FeatureTypeMapping', namespaces=ns):
   name=featureType.get('name').replace(':','.') + '_'
   for fidMapping in featureType.xpath('./ns0:FIDMapping', namespaces=ns):
     prefix=fidMapping.get('prefix')
-    print prefix
     fidMapping.set('prefix',name)
-  print name
 
 tree.write(sys.stdout, encoding='utf-8', pretty_print=True)
