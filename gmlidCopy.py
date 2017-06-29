09b2865501f7cac69a9e64921d61710f9af9d899
jarped <jarle.pedersen@norconsult.com>
2017-06-29 10:51:59 +0200

    remove unused variable

--- gmlidCopy.py
+++ gmlidCopy.py
@@ -9,7 +9,6 @@ tree = etree.parse(open(sys.argv[1]))
 for featureType in tree.xpath('//ns0:FeatureTypeMapping', namespaces=ns):
   name=featureType.get('name').replace(':','.') + '_'
   for fidMapping in featureType.xpath('./ns0:FIDMapping', namespaces=ns):
-    prefix=fidMapping.get('prefix')
     fidMapping.set('prefix',name)
 
 tree.write(sys.stdout, encoding='utf-8', pretty_print=True)
